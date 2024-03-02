0b1d1729af0a0fd7d9cdf46828f4090f85ebe2c5
unknown <clarissemtcarvalho@gmail.com>
2024-03-02 17:21:31 +0000

    Add more patterns, working road infinity

--- Assets/Scripts/SectionTrigger.cs
+++ Assets/Scripts/SectionTrigger.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class SectionTrigger : MonoBehaviour
 {
-    public GameObject roadSection;
+    public GameObject[] roadSections; // Array of prefabs to choose from
+    private float sectionSpacing = 40f; // Spacing between sections along the z-axis
 
     private void OnTriggerEnter(Collider other) 
     {
         if(other.gameObject.CompareTag("Trigger")) 
         {
-            Instantiate(roadSection, new Vector3(11, 2, transform.position.z + 61), Quaternion.identity);
+            // Check if there are road sections to choose from
+            if (roadSections.Length > 0)
+            {
+                // Calculate the new z position based on the number of instantiated sections
+                // float newZ = transform.position.z + sectionSpacing;
+                float newZ = 60;
+                // Randomly select a prefab from the array
+                int randomIndex = Random.Range(0, roadSections.Length);
+                GameObject selectedRoadSection = roadSections[randomIndex];
+                Debug.Log(selectedRoadSection + "|" + sectionSpacing + "|" + transform.position.z + "|" + newZ);
+
+                // Instantiate the selected prefab with the new position
+                Instantiate(selectedRoadSection, new Vector3(0, 0, newZ), Quaternion.identity);
+            }
+            else
+            {
+                Debug.LogWarning("No road sections available.");
+            }
         }
     }
 }
