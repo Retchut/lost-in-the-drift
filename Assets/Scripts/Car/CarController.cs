c9ceaf008dc1d1106b77a5ae441e16bda20ef8b4
Mário Travassos <retchut@protonmail.ch>
2024-03-02 21:21:29 +0000

    feat: pass car velocity to speed feedback shader

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -2,13 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
-    public enum Axel {Front, Rear}
+    public enum Axel { Front, Rear }
 
     [System.Serializable]
-    public struct Wheel {
+    public struct Wheel
+    {
         public GameObject wheelModel;
         public WheelCollider wheelCollider;
         public Axel axel;
@@ -27,52 +29,74 @@ public class CarController : MonoBehaviour
     public Vector3 _centerOfMass;
 
     public List<Wheel> wheelList;
-    private Rigidbody carRigidBody; 
+    private Rigidbody carRigidBody;
 
     private string verticalAxis = "Vertical";
     private string horizontalAxis = "Horizontal";
 
-    private void Start() {
+    [SerializeField]
+    private Material renderTextureMaterial;
+
+    private void Start()
+    {
         carRigidBody = GetComponent<Rigidbody>();
         carRigidBody.centerOfMass = _centerOfMass;
+        GameObject renderOutputObj = GameObject.FindWithTag("RenderOutput");
+        if (renderOutputObj)
+        {
+            renderTextureMaterial = renderOutputObj.GetComponent<RawImage>().material;
+        }
     }
 
-    private void Update() {
+    private void Update()
+    {
         GetInputs();
         AnimateWheels();
     }
 
-    private void LateUpdate() {
+    private void LateUpdate()
+    {
         Move();
+        if (renderTextureMaterial)
+            renderTextureMaterial.SetFloat("_CarSpeed", carRigidBody.velocity.magnitude);
     }
 
-    private void GetInputs() {
+    private void GetInputs()
+    {
         moveInput = Input.GetAxis(verticalAxis);
         steerInput = Input.GetAxis(horizontalAxis);
     }
 
     // TODO: remove magic numbers
-    private void Move() {
-        foreach(var wheel in wheelList) {
+    private void Move()
+    {
+        foreach (var wheel in wheelList)
+        {
             wheel.wheelCollider.motorTorque = moveInput * maxAcceleration * Time.deltaTime;
-            
+
             // Steer
-            if (wheel.axel == Axel.Front) {
+            if (wheel.axel == Axel.Front)
+            {
                 var _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
                 wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, steerLerpInterpolationValue);
             }
 
             // Brake
-            if (Input.GetKey(KeyCode.Space)) {
+            if (Input.GetKey(KeyCode.Space))
+            {
                 wheel.wheelCollider.brakeTorque = brakeAcceleration * Time.deltaTime;
-            } else {
+            }
+            else
+            {
                 wheel.wheelCollider.brakeTorque = 0;
             }
         }
     }
 
-    private void AnimateWheels() {
-        foreach(var wheel in wheelList) {
+    private void AnimateWheels()
+    {
+        foreach (var wheel in wheelList)
+        {
             wheel.wheelCollider.GetWorldPose(out Vector3 _position, out Quaternion _rotation);
             wheel.wheelModel.transform.SetPositionAndRotation(_position, _rotation);
         }
