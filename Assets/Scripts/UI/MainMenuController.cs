f19f85009c317aa913945d00c63cddd119f2d0f8
Mário Travassos <retchut@protonmail.ch>
2024-03-03 16:39:14 +0000

    fix: fix canvas resolution scaling

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -9,6 +9,7 @@ public class MainMenuController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Screen.SetResolution(1000, 750, true);
         GameObject renderOutputObj = GameObject.FindWithTag("RenderOutput");
         if (renderOutputObj)
         {
