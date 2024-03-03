77c85c0fd39e20b8e2440f580b5033f0e72d2e91
Mário Travassos <retchut@protonmail.ch>
2024-03-03 05:45:12 +0000

    feat: implement NPCar movement alongside road

--- Assets/Scripts/NPCar/NPCarController.cs
+++ Assets/Scripts/NPCar/NPCarController.cs
@@ -1,31 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NPCarController : MonoBehaviour
 {
-    [SerializeField]
-    private List<Transform> wpPositions;
+    private Queue<Transform> waypoints;
 
+    [SerializeField]
     private float baseSpeed = 10.0f;
+    [SerializeField]
     private float acceleration = 5.0f;
+    private Vector3 nextWaypointPos; // checkpoint we are navigating to
+
+    private float DEG_TO_RAD = Mathf.PI / 180;
+    private float RAD_TO_DEG = 180 / Mathf.PI;
 
     // Start is called before the first frame update
     void Start()
     {
-        wpPositions = new List<Transform>();
+        nextWaypointPos = Vector3.zero;
+        waypoints = new Queue<Transform>();
         // initialize waypoints from tiles already present when the NPCar is instantiated
         // iterating through containers, then through their children, to make sure we get the order right
         //      Note: the FindBlaBlaBla methods iterate from the top of the hierarchy down. So tiles lower down in the inspector were instantiated later!
+        // There's also no need to update the checkpoints, as any additional checkpoint that might be created comes with tiles the NPCar can never travel to
         GameObject[] wpContainers = GameObject.FindGameObjectsWithTag("WaypointContainer");
+        Transform prevWaypoint = null;  // since Peeking a queue gives us the next item to dequeue, not the last enqueued item, we need to store the previously enqueued element
         foreach (GameObject container in wpContainers)
         {
-            foreach (Transform wp in container.transform)
+            // this should also theoretically work with a stack, and going up in indices instead of down
+            // but I am way too tired to make it work, and this works so let's keep it that way :)
+            for (int i = container.transform.childCount - 1; i >= 0; i--)
             {
-                Debug.Log(wp.transform.position);
-                wpPositions.Add(wp.transform);
+                // can't peek an empty queue duh
+                if (waypoints.Count == 0)
+                {
+                    waypoints.Enqueue(container.transform.GetChild(i));
+                    prevWaypoint = container.transform.GetChild(i);
+                    continue;
+                }
+
+                // waypoints overlap at the start and end of the tiles, so we need to filter them out, and only keep the non-overlapping ones (usually filter the last waypoint of each tile)
+                Transform currWaypoint = container.transform.GetChild(i);
+                if (prevWaypoint.transform.position != currWaypoint.transform.position)
+                {
+                    waypoints.Enqueue(container.transform.GetChild(i));
+                    prevWaypoint = container.transform.GetChild(i);
+                }
             }
         }
+        // skip the last waypoint of the first tile, because the NPCar is instantiated ahead of it
+        waypoints.Dequeue();
+        nextWaypointPos = waypoints.Dequeue().position;
     }
 
     // Update is called once per frame
@@ -33,5 +61,36 @@ public class NPCarController : MonoBehaviour
     {
         baseSpeed += acceleration * Time.fixedDeltaTime;
         transform.position += transform.forward * baseSpeed * Time.fixedDeltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // hit a waypoint
+        if (other.gameObject.CompareTag("Waypoint"))
+        {
+            // only do stuff when triggering for the first time with a waypoint with the next intended position
+            // if we've already hit one of the overlapping triggers once, we've already dequeued and obtained the next position
+            if (other.transform.position == nextWaypointPos)
+            {
+                // all waypoints were passed
+                if (waypoints.Count == 0)
+                {
+                    return;
+                }
+                nextWaypointPos = waypoints.Dequeue().position;
+
+                // calculate angle to next waypoint pos
+                Vector3 dirToNextWaypoint = new Vector3(nextWaypointPos.x, 0.0f, nextWaypointPos.z) - new Vector3(transform.position.x, 0.0f, transform.position.z); // ignoring y coordinate variation
+                float angle = Vector3.Angle(dirToNextWaypoint, transform.forward);
+                float angleRad = angle * DEG_TO_RAD;
+
+                // between a certain threshold (around 75º), we consider the NPCar needs to turn
+                if (Mathf.Cos(angleRad) <= 0.25)
+                {
+                    Quaternion rotation = Quaternion.LookRotation(nextWaypointPos - transform.position);
+                    transform.rotation = rotation;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Road/RoadController.cs
+++ Assets/Scripts/Road/RoadController.cs
@@ -3,21 +3,6 @@ using UnityEngine;
 
 public class RoadController : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform != null)
-        {
-            transform.position += Vector3.back * Time.deltaTime;
-        }
-        else
-        {
-            // Handle the case where the transform is null, for example:
-            Debug.LogWarning("Transform is null!");
-        }
-
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Destroy"))
